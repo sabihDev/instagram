c86e72ec260ff0c188c61ac357623bf188e32371
sabihDev <8203sabi.rahman@gmail.com>
2024-08-10 16:10:27 +0500

    cleaning project and starting from scratch

--- backend/connect.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
- 
-function Connect(url) {
-    mongoose.connect(url)
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log(err));
-}
-
-module.exports = Connect;
