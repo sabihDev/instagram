665915fe8abaa66f33c1f50d8867b8f94b47f812
sabihDev <8203sabi.rahman@gmail.com>
2024-08-12 18:11:37 +0500

    frontend setuped

---
+++ frontend/src/components/Signup.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div className='flex w-screen h-screen items-center justify-center'>
+        <form action="">
+            <div>LOGO</div>
+            <p>Signup to see photos and videos of your friends</p>
+        </form>
+    </div>
+  )
+}
+
+export default Signup
